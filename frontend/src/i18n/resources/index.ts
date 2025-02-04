366157f873e4684aac741495c92a5dd07fc84fda
parkjeehoon <gkstj8300@naver.com>
2025-02-04 10:27:27 +0900

    chore: i18n 설정 완료 및 frontEnd 패키지 구조 변경

---
+++ frontend/src/i18n/resources/index.ts
@@ -0,0 +1,5 @@
+import { en } from './en';
+import { ko } from './ko';
+import { Resources } from '@/i18n/types';
+
+export const resources: Resources = { en, ko };
---
+++ frontend/src/i18n/resources/ko/translation/index.ts
@@ -0,0 +1,13 @@
+import type { Translation } from '@/i18n/types';
+
+const translation: Translation = {
+	common: {
+	},
+	component: {
+		ui: {
+		}
+	}
+};
+
+// 'export default' for i18n-ally (vscode extension)
+export default translation;
