a78de2b7da8bf565661b5c768963da6da7ab668e
parkjeehoon <gkstj8300@naver.com>
2025-02-07 10:36:49 +0900

    fix: correct spelling mistake in text
    - 포트폴리오 spelling 수정 fortpolio -> portfolio

--- frontend/src/components/pages/Home/Contact/Contact.i18n.ko.ts
+++ frontend/src/components/pages/Home/Contact/Contact.i18n.ko.ts
@@ -3,7 +3,7 @@ import { Translation } from '@/i18n/types';
 export const contact: Translation = {
     email: 'email',
     github: 'github',
-    portfolio: 'fortpolio',
+    portfolio: 'portfolio',
     emailTooltip: '이메일을 클릭하시면 복사가 가능합니다.',
     emailCopy: '이메일 주소가 복사되었습니다.',
 }
