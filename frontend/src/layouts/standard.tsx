06d6d13e0f72de796feee0d9dc91e988dffe2290
parkjeehoon <gkstj8300@naver.com>
2025-02-11 15:40:24 +0900

    fix: standart layout 버그 수정
    - layout 컴포넌트 조정으로 인한 header, footer 중복 출력 수정

--- frontend/src/layouts/standard.tsx
+++ frontend/src/layouts/standard.tsx
@@ -1,6 +1,5 @@
-import { Layout } from '@/components/layout/Layout';
 import { GetLayout } from '@/utils/types';
 
 export const Standard: GetLayout = page => {
-    return <Layout>{page}</Layout>;
+    return <>{page}</>;
 }
