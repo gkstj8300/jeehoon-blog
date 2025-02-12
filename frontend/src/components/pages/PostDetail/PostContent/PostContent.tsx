103fd8120fc00cb16cd3d9f077ac7032a002f618
parkjeehoon <gkstj8300@naver.com>
2025-02-12 19:00:53 +0900

    feat: ReactMarkdown 적용 진행중

--- frontend/src/components/pages/PostDetail/PostContent/PostContent.tsx
+++ frontend/src/components/pages/PostDetail/PostContent/PostContent.tsx
@@ -1,5 +1,9 @@
 import styles from './PostContent.module.scss';
 import { markDownContentFormat } from '@/utils/markDown/markDown';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 type Props = {
     content: string;
@@ -9,7 +13,11 @@ export const PostContent: React.FC<Props> = ({ content }) => {
     const markDownContent = markDownContentFormat(content);
     return (
         <div className={styles.container}>
-            {markDownContent}
+            <ReactMarkdown 
+                remarkPlugins={[remarkGfm]}
+            >
+                {markDownContent}
+            </ReactMarkdown>
         </div>
     );
 };
