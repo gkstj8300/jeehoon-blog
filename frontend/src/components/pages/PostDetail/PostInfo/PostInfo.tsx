9aadb12b795f3953f3e731ca0d8fe3ff1dbcf4f4
parkjeehoon <gkstj8300@naver.com>
2025-02-25 15:05:47 +0900

    style: 게시글 상세 css 수정

--- frontend/src/components/pages/PostDetail/PostInfo/PostInfo.tsx
+++ frontend/src/components/pages/PostDetail/PostInfo/PostInfo.tsx
@@ -11,12 +11,15 @@ export const PostInfo: React.FC<Props> = ({
     mainTag,
     tags,
 }) => {
+    const formattedDate = regDate.replace(/-/g, '.');
     return (
         <div className={styles.container}>
             <div className={styles.info}>
-                <div className={styles.author}>@BaakHan</div>
-                <div className={styles.regDate}>{regDate}</div>
                 <div className={styles.mainTag}>{mainTag}</div>
+                <div className={styles.subInfo}>
+                    <div className={styles.author}>@BaakHan</div>
+                    <div className={styles.regDate}>{formattedDate}</div>
+                </div>
             </div>
             <div className={styles.tags}>
                 {tags.map((tag, index) => (
