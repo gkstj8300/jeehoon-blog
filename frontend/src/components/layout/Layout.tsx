3c0c5ad18b44c8d689861cae4df78f6a90e63d9f
parkjeehoon <gkstj8300@naver.com>
2025-02-26 15:15:00 +0900

    fix: Notice(공지) 컴포넌트 제거

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -3,7 +3,6 @@ import styles from './Layout.module.scss';
 import { Footer } from './footer';
 import { BackToTop } from './footer/BackToTop';
 import { Header } from './header';
-import { Notice } from './notice';
 import ErrorBoundary from '@/components/boundaries/ErrorBoundary';
 import { ContextProviders } from '@/components/functional/ContextProviders';
 import { TooltipController } from '@/components/ui/tooltips';
@@ -16,7 +15,6 @@ export const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
             <ContextProviders>
                 <div className={styles.container} ref={rootRef}>
                     <Header />
-                    <Notice />
                     <div className={styles.layout}>
                         {children}
                         <div className={styles.backToTop}>
