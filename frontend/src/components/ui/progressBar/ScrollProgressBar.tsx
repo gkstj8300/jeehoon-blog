9a2ccfe8d00460289cccee431f3abe1912061b14
parkjeehoon <gkstj8300@naver.com>
2025-02-27 16:03:23 +0900

    feat: ScrollProgressBar 구현 작업

---
+++ frontend/src/components/ui/progressBar/ScrollProgressBar.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState, useRef } from "react";
+import styles from './ScrollProgressBar.module.scss';
+
+export const ScrollProgressBar: React.FC = () => {
+    const [scrollPercentage, setScrollPercentage] = useState(0);
+    const progressBarRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        let animationFrameId: number;
+
+        const updateScrollProgress = () => {
+            const scrollTop = window.scrollY;
+            const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+            const progress = scrollTop / scrollHeight;
+
+            setScrollPercentage(progress);
+            animationFrameId = requestAnimationFrame(updateScrollProgress);
+        };
+
+        const onScroll = () => {
+            if (!animationFrameId) {
+                animationFrameId = requestAnimationFrame(updateScrollProgress);
+            }
+        };
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => {
+            window.removeEventListener("scroll", onScroll);
+            cancelAnimationFrame(animationFrameId);
+        };
+    }, []);
+
+    const handleProgressBarClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (!progressBarRef.current) return;
+
+        const { left, width } = progressBarRef.current.getBoundingClientRect();
+        const clickX = e.clientX - left;
+        const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+        const targetScroll = (clickX / width) * scrollHeight;
+        window.scrollTo({ top: targetScroll, behavior: "smooth" });
+    };
+
+    return (
+        <div 
+            className={styles.container} 
+            ref={progressBarRef} 
+            onClick={handleProgressBarClick} // 클릭 이벤트 추가
+        >
+            <div 
+                className={styles.scrollProgressBar} 
+                style={{
+                    transform: `scaleX(${scrollPercentage})`,
+                    transformOrigin: "left",
+                }}
+            />
+        </div>
+    );
+};
+ScrollProgressBar.displayName = 'ScrollProgressBar';
