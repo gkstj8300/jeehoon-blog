b36f253be7a6866cf0e5ccdded6371ef6c9748f2
parkjeehoon <gkstj8300@naver.com>
2025-03-18 23:22:45 +0900

    fix: onClick 이벤트 실행시 페이지 상단으로 이동하는 버그 수정

--- frontend/src/components/ui/links/TagLink.tsx
+++ frontend/src/components/ui/links/TagLink.tsx
@@ -31,6 +31,7 @@ export const TagLink: FC<Props> = forwardRef<HTMLAnchorElement, Props>(
 				ref={ref}
                 className={classNames(styles.link, className)}
                 {...props}
+				onClick={(e) => e.preventDefault()}
             >
 				<>
 				{name}
