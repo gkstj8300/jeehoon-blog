53c4359e9ac99d5ec48ddcb401a07657f69d9408
parkjeehoon <gkstj8300@naver.com>
2025-04-06 00:37:11 +0900

    feat: 게시글 상세보기 버튼 작업

--- frontend/src/components/pages/PostList/PostList.tsx
+++ frontend/src/components/pages/PostList/PostList.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import { PostContent } from './PostContent';
 import { PostInfo } from './PostInfo';
 import styles from './PostList.module.scss';
 import { PostTitle } from './PostTitle';
 import { Breadcrumbs } from '@/components/ui/links/Breadcrumbs';
 import { PostType } from "@/models/pages/slug";
+import { url } from '@/utils/url';
 
 type Props = {
     postList: PostType[];
@@ -25,14 +27,20 @@ export const PostList: React.FC<Props> = ({ postList }) => {
                     해당 영역은 검색창 영역입니다.
                 </div>
                 <div className={styles.postListWrap}>
-                    {postList.map((post, index) => (
-                        <div key={index} className={styles.post}>
+                    {postList.map((post) => (
+                        <div key={post.title} className={styles.post}>
                             <PostTitle title={post.title}/>
                             <PostInfo 
                                 regDate={post.regDate} 
                                 tags={post.tags} 
                             />
                             <PostContent content={post.content}/>
+                            <div className={styles.detailButton}>
+                                <div className={styles.overlay}></div>
+                                <Link className={styles.button} href={url.postDetail(post.slug)}>
+                                    상세보기
+                                </Link>
+                            </div>
                         </div>
                     ))}
                 </div>
