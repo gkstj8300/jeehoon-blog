76c8b8ecb1c794f05b31021ecfeb338724374535
parkjeehoon <gkstj8300@naver.com>
2025-04-05 21:57:22 +0900

    feat: PostContent componenet 작업 및 적용

--- frontend/src/components/pages/PostList/PostList.tsx
+++ frontend/src/components/pages/PostList/PostList.tsx
@@ -1,7 +1,43 @@
-export const PostList: React.FC = () => {
+import { PostContent } from './PostContent';
+import { PostInfo } from './PostInfo';
+import styles from './PostList.module.scss';
+import { PostTitle } from './PostTitle';
+import { Breadcrumbs } from '@/components/ui/links/Breadcrumbs';
+import { PostType } from "@/models/pages/slug";
+
+type Props = {
+    postList: PostType[];
+};
+
+export const PostList: React.FC<Props> = ({ postList }) => {
 
     return (
-        <></>
+        <>
+            <Breadcrumbs
+                className={styles.breadcrumb}
+                breadcrumbList={[{
+                    text: '전체글',
+                    strong: true,
+                }]}
+            />
+            <div className={styles.container}>
+                <div className={styles.searchWrap}>
+                    해당 영역은 검색창 영역입니다.
+                </div>
+                <div className={styles.postListWrap}>
+                    {postList.map((post, index) => (
+                        <div key={index} className={styles.post}>
+                            <PostTitle title={post.title}/>
+                            <PostInfo 
+                                regDate={post.regDate} 
+                                tags={post.tags} 
+                            />
+                            <PostContent content={post.content}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 };
 PostList.displayName = 'PostList';
