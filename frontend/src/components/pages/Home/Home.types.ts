ccaa9cc5a90bedc69bf0ac45d4947b030b1e8df3
parkjeehoon <gkstj8300@naver.com>
2025-08-15 01:38:50 +0900

    fix: 패키지 대소문자 변경으로 인한 제거

--- frontend/src/components/pages/Home/Home.types.ts
+++
@@ -1,6 +0,0 @@
-export type Sort = 'desc' | 'asc';
-
-export type SearchType = {
-	keyword?: string;
-	tag?: string;
-};
--- frontend/src/components/pages/Home/RecentPosts/RecentPosts.i18n.ko.ts
+++
@@ -1,7 +0,0 @@
-import { Translation } from '@/lib/i18n/types';
-
-export const recentPosts: Translation = {
-	recentPost: '최신 글',
-	allPost: '전체 보기',
-	regDate: '생성일: {{date}}',
-};
