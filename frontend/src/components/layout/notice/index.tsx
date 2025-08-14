2f8a17a7dc1c4074a13411475b547a594ee60b52
parkjeehoon <gkstj8300@naver.com>
2025-08-14 18:02:02 +0900

    fix: 패키지 대소문자 변경으로 인한 제거

--- frontend/src/components/layout/notice/index.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './Notice.module.scss';
-
-export default function Notice() {
-	return (
-		<div className={styles.container}>현재 블로그 리펙토링 작업중입니다.</div>
-	);
-}
-Notice.displayName = 'Notice';
